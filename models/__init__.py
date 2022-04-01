97b12daeea252ccebbad4cb42829c9762048492a
EA264728 <elouan.argouarch@cea.fr>
2022-04-01 14:37:06 +0200

    wip

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,2 @@
 from .neural_ratio_density_estimator import NDRE
 from .conditional_neural_ratio_density_estimator import CNDRE
-from .adversarial_density_estimation import AdversarialDensityEstimator
-from .generative_adversarial_network import GenerativeAdversarialNetwork
